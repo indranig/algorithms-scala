a5a1df7054ca8a19352ef5b5a03cfeb29a2980ad
awong <alan_kt_wong@yahoo.com>
2014-08-21 11:22:57 -0400

    More work

--- searching/src/test/scala/org/awong/searching/tree/BinaryTreeSpec.scala
+++ searching/src/test/scala/org/awong/searching/tree/BinaryTreeSpec.scala
@@ -31,27 +31,39 @@ class BinaryTreeSpec extends AbstractWordSpec {
 		}
 		
 		"be able to create a 3-node tree and add/remove an element" in {
-				var xs = Seq(2,1,3)
-				var tree = BinarySearchTree(xs)
-				tree.isEmpty should be (false)
-				tree.isValid should be (true)
-				tree.isBalanced should be (true)
-				tree.size should be (xs.size)
-				
-				tree.contains(util.Random.shuffle(xs).head) should be (true)
-				
-				val add = 4
-				xs = xs :+ add
-				tree = tree.add(add)
-				tree.size should be (xs.size)
-				tree.contains(add) should be (true)
-				tree.isValid should be (true)
-				tree.isBalanced should be (true)
-				
-				tree = tree.remove(add)
-				xs = xs.filterNot( each => each == add)
-				tree.size should be (xs.size)
+			var xs = Seq(2,1,3)
+			var tree = BinarySearchTree(xs)
+			tree.isEmpty should be (false)
+			tree.isValid should be (true)
+			tree.isBalanced should be (true)
+			tree.size should be (xs.size)
+			
+			tree.contains(util.Random.shuffle(xs).head) should be (true)
+			
+			val add = 4
+			xs = xs :+ add
+			tree = tree.add(add)
+			tree.size should be (xs.size)
+			tree.contains(add) should be (true)
+			tree.isValid should be (true)
+			tree.isBalanced should be (true)
+			
+			tree = tree.remove(add)
+			xs = xs.filterNot( each => each == add)
+			tree.size should be (xs.size)
+			
+		}
+		
+		"have correct subtree functionality" in {
+			var tree = BinarySearchTree(Seq(2,1,3))
+			val subtree = tree.subtree(1)
+			subtree.isEmpty should be (false)
+			subtree.left.isEmpty should be (true)
+			subtree.right.isEmpty should be (true)
+			tree.isSubtree(BinarySearchTree.empty) should be (true)
+			tree.isSubtree(subtree) should be (true)
+			val totallyDifferent = BinarySearchTree(Seq(4))
+			tree.isSubtree(totallyDifferent) should be (false)
 		}
 	}
-
 }
